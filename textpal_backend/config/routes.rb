d4911880b59cc52bd8e732e512dbb8ae4abfa791
Felix <felix.benetzeder@blackrock.com>
2019-07-09 08:59:36 +0100

    login

--- textpal_backend/config/routes.rb
+++ textpal_backend/config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :comments, only: [:create, :destroy]
   resources :likes, only: [:create, :destroy]
   resources :projects, only: [:create, :update, :destroy]
-  resources :users, only: [:create]
+  resources :users, only: [:create, :index]
 end
